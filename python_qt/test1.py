9c05cfb8d569d4ac2200149f8f7f05db685a5375
dzxx301 <flj@bgy.edu.cn>
2020-06-05 11:16:04 +0800

    增加qt文件夹和

---
+++ python_qt/test1.py
@@ -0,0 +1,10 @@
+# !/usr/bin/python
+# -*- coding:utf-8 -*-
+
+from tkinter import *
+
+def gui_start():
+    init_window = Tk()        #实例化出一个父窗口
+    init_window.mainloop()    #父窗口进入事件循环，可以理解为保持窗口运行，否则界面不展示
+
+gui_start()
