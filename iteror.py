9aa323eded36edaf3a76e0dda7fe7663376fddb3
dzxx301 <flj@bgy.edu.cn>
2020-06-18 16:55:33 +0800

    增加一个实习迭代器文件

---
+++ iteror.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+#import sys  # 引入 sys 模块
+
+list = [1, 2, 3, 4]
+it = iter(list)  # 创建迭代器对象
+
+while True:
+    try:
+        print(next(it))
+    except StopIteration:
+        #sys.exit()
+        break
